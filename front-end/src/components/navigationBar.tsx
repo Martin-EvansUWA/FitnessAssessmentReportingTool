77bae68f903a26cae4ca8f5ab8e0c1472608d2d8
LloydN01 <lloydna0527@gmail.com>
2024-08-18 11:21:23 +0800

    make navigation bar responsive

--- front-end/src/components/navigationBar.tsx
+++ front-end/src/components/navigationBar.tsx
@@ -13,42 +13,47 @@ const NavigationBar = () => {
     return (
         <>
             <div className="relative w-full h-16">
-                <nav className="flex justify-between items-center px-4 bg-uwa-blue rounded-full font-bold w-full h-16">
-                    <div className="ml-auto flex items-center space-x-2">
-                        <span className="text-white">
-                            Hello, Placeholder12345678!
-                        </span>
-                        <FontAwesomeIcon
-                            icon={faUser}
-                            className="text-3xl text-white"
-                        />
-                    </div>
-                </nav>
-                <nav className="absolute top-0 left-0 flex justify-between items-center px-4 bg-white rounded-full font-bold lg:w-[80%] h-16 z-10">
+                <nav className="absolute top-0 left-0 flex justify-between items-center pl-4 bg-white rounded-full font-bold w-full h-16 z-10">
                     <div className="flex items-center">
                         <a
                             href="#landing-page"
                             className="inline-flex items-center"
                         >
                             <img src={uwaLogo} alt="UWA Logo" className="h-8" />
-                            <span className="ml-5 text-uwa-blue">
+                            <span className="ml-5 text-uwa-blue text-sm md:text-base mr-10">
                                 Fitness Assessment Reporting Tool
                             </span>
                         </a>
                     </div>
-                    <div className="hidden md:flex space-x-10 h-full items-center">
-                        <a href="#tutorial" className="text-uwa-blue">
+                    <div className="hidden md:flex space-x-5 h-full items-center">
+                        <a
+                            href="#tutorial"
+                            className="text-uwa-blue text-sm md:text-base"
+                        >
                             Tutorial
                         </a>
-                        <a href="#about" className="text-uwa-blue">
+                        <a
+                            href="#about"
+                            className="text-uwa-blue text-sm md:text-base"
+                        >
                             Logout
+                        </a>
+                        <a className="flex flex-row items-center space-x-2 bg-uwa-blue h-full rounded-r-full">
+                            <div className="bg-white rounded-r-full w-10 h-full"></div>
+                            <span className="text-white text-sm md:text-base">
+                                Hello, Placeholder!
+                            </span>
+                            <FontAwesomeIcon
+                                icon={faUser}
+                                className="text-3xl text-white pr-5"
+                            />
                         </a>
                     </div>
                     <div className="md:hidden flex items-center">
                         <button
                             title="navigation-button"
                             onClick={toggleMenu}
-                            className="focus:outline-none"
+                            className="focus:outline-none text-uwa-blue"
                         >
                             {/* Asset from W3C for menu icon. Change in future if needed */}
                             <svg
@@ -69,11 +74,23 @@ const NavigationBar = () => {
                     </div>
                 </nav>
                 {isMenuOpen && (
-                    <div className="md:hidden flex flex-col items-center bg-white border-solid border-2 border-black rounded-lg mt-2 p-4 space-y-4 float-right mx-1">
-                        <a href="#tutorial" onClick={toggleMenu}>
+                    <div className="md:hidden flex flex-col bg-uwa-blue rounded-lg mt-16 p-4 space-y-5 float-right mx-1 absolute top-0 right-0 z-10">
+                        <span className="text-white text-sm font-bold">
+                            Hello, Placeholder!
+                        </span>
+
+                        <a
+                            href="#tutorial"
+                            onClick={toggleMenu}
+                            className="text-sm text-white"
+                        >
                             Tutorial
                         </a>
-                        <a href="#about" onClick={toggleMenu}>
+                        <a
+                            href="#about"
+                            onClick={toggleMenu}
+                            className="text-sm text-white"
+                        >
                             Logout
                         </a>
                     </div>
