e22e570773c2f3755b26f3bdeb6faa59325eb09d
LloydN01 <lloydna0527@gmail.com>
2024-08-18 15:03:51 +0800

    add grids to side bar for visual balance

--- front-end/src/components/sidebar.tsx
+++ front-end/src/components/sidebar.tsx
@@ -41,38 +41,44 @@ const Sidebar = ({
     return (
         <aside
             className={
-                "sidebar w-full rounded-2xl p-5 bg-uwa-blue" + " " + className
+                "sidebar w-full rounded-2xl p-5 bg-uwa-blue grid grid-rows-10 grid-flow-col gap-4" +
+                " " +
+                className
             }
         >
-            <h2 className="text-white font-bold">{content.title}</h2>
-            <hr className="w-28 border-t-2 border-uwa-yellow my-2" />
-            <div>
-                {content.sections.map((section, index) => (
-                    <ul key={index}>
-                        {Object.entries(section).map(([key, value]) => (
-                            <li key={key}>
-                                <button
-                                    onClick={value.sectionOnClick}
-                                    className="text-white"
-                                >
-                                    {value.sectionName}
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                ))}
+            <div className="row-start-1 row-end-10">
+                <h2 className="text-white font-bold">{content.title}</h2>
+                <hr className="w-28 border-t-2 border-uwa-yellow my-2" />
+                <div>
+                    {content.sections.map((section, index) => (
+                        <ul key={index}>
+                            {Object.entries(section).map(([key, value]) => (
+                                <li key={key} className="my-3">
+                                    <button
+                                        onClick={value.sectionOnClick}
+                                        className="text-white"
+                                    >
+                                        {value.sectionName}
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                    ))}
+                </div>
             </div>
-            <hr className="w-28 border-t-2 border-uwa-yellow my-2" />
-            <button
-                onClick={content.footer.onClick}
-                className="text-white font-bold flex items-end"
-            >
-                {content.footer.text}
-                <FontAwesomeIcon
-                    icon={content.footer.fontAwesomeIcon}
-                    className="ml-2 text-xl"
-                />
-            </button>
+            <div className="row-start-10">
+                <hr className="w-28 border-t-2 border-uwa-yellow my-2" />
+                <button
+                    onClick={content.footer.onClick}
+                    className="text-white font-bold flex items-end"
+                >
+                    {content.footer.text}
+                    <FontAwesomeIcon
+                        icon={content.footer.fontAwesomeIcon}
+                        className="ml-2 text-xl"
+                    />
+                </button>
+            </div>
         </aside>
     );
 };
