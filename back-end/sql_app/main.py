e931a2703955028da56ba1f95e44336f9ed4b27a
Jericho Cura <100751647+hurrkage@users.noreply.github.com>
2024-08-20 15:45:55 +0800

    removed some dependencies

--- back-end/sql_app/main.py
+++ back-end/sql_app/main.py
@@ -1,33 +1,25 @@
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
+from starlette.responses import FileResponse
+import json
 
-from . import crud, models, schemas
-from .database import SessionLocal, engine
-
-models.Base.metadata.create_all(bind=engine)
-
+#app implementation
 app = FastAPI()
 
+@app.get("/")
+def index():
+    return "temp"
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+@app.get("/student")
+def student():
+    return "student"
 
+@app.get("/admin")
+def student():
+    return "admin"
 
-@app.get("/student/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="Student not found")
-    return db_user
-
-@app.get("/admin/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="Admin not found")
-    return db_user
+@app.post("/form")
+def form(form_json):
+    form_data = json.loads(form_json)
+    for key in form_data:
+        print(key + ": " + str(form_data[key]))
+    return "form"
