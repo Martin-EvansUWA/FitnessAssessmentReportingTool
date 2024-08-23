f82c632a55fdce1b8df309f82c666cb4bf1e2d3b
LloydN01 <lloydna0527@gmail.com>
2024-08-23 23:10:32 +0800

    add state for data entry to keep context

--- front-end/src/components/formTemplate.tsx
+++ front-end/src/components/formTemplate.tsx
@@ -12,6 +12,7 @@ const FormTemplate = ({
     previousSection,
     nextSection,
     formTemplate,
+    formData,
     onNextPage,
     onPreviousPage,
     onInputChange,
@@ -19,6 +20,7 @@ const FormTemplate = ({
     previousSection: string | null;
     nextSection: string | null;
     formTemplate: FormTemplateInterface;
+    formData: { [key: string]: { [key: string]: any } };
     onNextPage: () => void;
     onPreviousPage: () => void;
     onInputChange: (
@@ -32,73 +34,77 @@ const FormTemplate = ({
         field: string,
         event: React.ChangeEvent<HTMLInputElement>
     ) => {
-        const typeOfInput = event.target?.type;
-        if (typeOfInput === "checkbox") {
-            onInputChange(section, field, event.target?.checked);
-        } else if (typeOfInput === "number") {
-            onInputChange(section, field, parseFloat(event.target?.value));
-        } else {
-            onInputChange(section, field, event.target?.value);
-        }
+        const { type, checked, value } = event.target;
+        const parsedValue =
+            type === "checkbox"
+                ? checked
+                : type === "number"
+                ? parseFloat(value)
+                : value;
+        onInputChange(section, field, parsedValue);
     };
+
     return (
         <div className="flex flex-col min-h-full justify-between">
             <div className="overflow-y-auto max-h-[100%]">
-                {Object.keys(formTemplate).map((section, index) => (
-                    <div key={index}>
+                {Object.keys(formTemplate).map((section) => (
+                    <div key={section}>
                         <h1 className="text-2xl font-bold mb-5">{section}</h1>
                         <hr className="w-28 border-t-2 border-uwa-yellow mt-2" />
                         <ul>
-                            {Object.keys(formTemplate[section]).map(
-                                (field, index) => (
-                                    <li
-                                        key={index}
-                                        className="my-2 flex flex-row justify-between items-center md:w-2/3"
-                                    >
-                                        <label className="font-bold">
-                                            {field}:
-                                        </label>
-                                        <input
-                                            type={
-                                                formTemplate[section][field] ===
-                                                MeasurementType.int
-                                                    ? "number"
-                                                    : formTemplate[section][
-                                                          field
-                                                      ] ===
-                                                      MeasurementType.float
-                                                    ? "number"
-                                                    : formTemplate[section][
-                                                          field
-                                                      ] === MeasurementType.bool
-                                                    ? "checkbox"
-                                                    : "text"
-                                            }
-                                            placeholder={
-                                                formTemplate[section][field] !==
-                                                MeasurementType.bool
-                                                    ? formTemplate[section][
-                                                          field
-                                                      ]
-                                                    : undefined
-                                            }
-                                            className={
-                                                formTemplate[section][field] !==
-                                                MeasurementType.bool
-                                                    ? "border border-gray-300 rounded-md p-1 w-1/3"
-                                                    : "border border-gray-300 rounded-md p-1 w-6 h-6"
-                                            }
-                                            onChange={(event) =>
-                                                handleChange(
-                                                    section,
-                                                    field,
-                                                    event
-                                                )
-                                            }
-                                        />
-                                    </li>
-                                )
-                            )}
+                            {Object.keys(formTemplate[section]).map((field) => (
+                                <li
+                                    key={field}
+                                    className="my-2 flex flex-row justify-between items-center md:w-2/3"
+                                >
+                                    <label className="font-bold">
+                                        {field}:
+                                    </label>
+                                    <input
+                                        type={
+                                            formTemplate[section][field] ===
+                                                MeasurementType.int ||
+                                            formTemplate[section][field] ===
+                                                MeasurementType.float
+                                                ? "number"
+                                                : formTemplate[section][
+                                                      field
+                                                  ] === MeasurementType.bool
+                                                ? "checkbox"
+                                                : "text"
+                                        }
+                                        placeholder={
+                                            formTemplate[section][field] !==
+                                            MeasurementType.bool
+                                                ? formTemplate[section][field]
+                                                : undefined
+                                        }
+                                        className={
+                                            formTemplate[section][field] !==
+                                            MeasurementType.bool
+                                                ? "border border-gray-300 rounded-md p-1 w-1/3"
+                                                : "border border-gray-300 rounded-md p-1 w-6 h-6"
+                                        }
+                                        checked={
+                                            formTemplate[section][field] ===
+                                            MeasurementType.bool
+                                                ? formData[section]?.[field] ||
+                                                  false
+                                                : undefined
+                                        }
+                                        value={
+                                            formTemplate[section][field] !==
+                                            MeasurementType.bool
+                                                ? formData[section]?.[field] ||
+                                                  ""
+                                                : undefined
+                                        }
+                                        onChange={(event) =>
+                                            handleChange(section, field, event)
+                                        }
+                                    />
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 ))}
