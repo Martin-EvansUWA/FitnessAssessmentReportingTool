2d7591ed17cfd67a0fc3127cefe0ec354d4799be
LloydN01 <lloydna0527@gmail.com>
2024-08-25 01:34:22 +0800

    redesign landing page background picture

--- front-end/src/pages/landingPage.tsx
+++ front-end/src/pages/landingPage.tsx
@@ -1,4 +1,6 @@
 import { ConditionalLinks } from "../App";
+import uwaSportScienceImg2 from "../assets/uwa-sport-science-2.jpg";
+import uwaSportScienceImg3 from "../assets/uwa-sport-science-3.jpg";
 import uwaSportScienceImg from "../assets/uwa-sport-science.jpg";
 import NavigationBar from "../components/navigationBar";
 
@@ -6,18 +8,28 @@ const LandingPage = () => {
     return (
         <div className="flex flex-col h-screen">
             <NavigationBar className="" />
+            <div className="absolute top-1/2 w-full bg-uwa-blue">
+                <p className="text-lg text-center font-bold text-white">
+                    This is a temporary landing page. In the future, this page
+                    will include the login/register components.
+                </p>
+                <ConditionalLinks className="text-white" />
+            </div>
             <div className="flex flex-1 flex-row justify-between w-full">
-                <div className="w-3/5">
-                    <p className="text-lg text-center font-bold">
-                        This is a temporary landing page. In the future, this
-                        page will include the login/register components.
-                    </p>
-                    <ConditionalLinks />
-                </div>
                 <img
                     src={uwaSportScienceImg}
                     alt="UWA Sport Science"
-                    className="hidden md:flex object-cover w-2/5"
+                    className="hidden md:flex object-cover w-1/3"
+                />
+                <img
+                    src={uwaSportScienceImg2}
+                    alt="UWA Sport Science"
+                    className="hidden md:flex object-cover w-1/3"
+                />
+                <img
+                    src={uwaSportScienceImg3}
+                    alt="UWA Sport Science"
+                    className="hidden md:flex object-cover w-1/3"
                 />
             </div>
         </div>
