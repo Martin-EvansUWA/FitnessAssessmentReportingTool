586f08f72041a28d1cc7ce4ca6d26cfc4c2a83e1
Martin-EvansUWA <23621647@student.uwa.edu.au>
2024-08-30 10:42:46 +0800

    feat: first test

--- back-end/sql_app/test_crud.py
+++ back-end/sql_app/test_crud.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker
 import pytest
 import models
 import schemas
+import crud
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./back-end/sql_app/test.db"
 
@@ -19,3 +20,15 @@ models.Base.metadata.create_all(bind=engine)
 
 
 def test_user_creation():
+    temp_user = schemas.DimUserCreate(
+        email="johnsmith1234@gmail.com",
+        FirstName="John",
+        LastName="Smith",
+        password="password1234",
+        StudentId=23621647
+    )
+    crud.create_DimUser(db,temp_user)
+    queried = crud.get_DimUser(db,23621647)
+
+    assert queried.FirstName == "John"
+    assert queried.LastName=="Smith"
