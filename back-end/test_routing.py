c1b3002ceb4614db6298d755eaeac7d20f47098e
Martin-EvansUWA <23621647@student.uwa.edu.au>
2024-09-13 12:16:11 +0800

    removed routing tests

--- back-end/test_routing.py
+++ back-end/test_routing.py
@@ -4,16 +4,6 @@ from app import app
 
 client = TestClient(app)
 
-def test_landing():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == 'temp'
-
-
-def test_student():
-    response = client.get("/student")
-    assert response.status_code == 200
-    assert response.json() == 'student'
 
 
 
