ea8074f9374f4e0c5038556c5ab7f61a74d1f955
LloydN01 <lloydna0527@gmail.com>
2024-09-23 00:27:22 +0800

    implement dummy sidebar content logic

--- front-end/src/pages/studentFormManagerPage.tsx
+++ front-end/src/pages/studentFormManagerPage.tsx
@@ -1,9 +1,10 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 import Layout from "../components/layout";
 import StudentFormManager from "../components/studentFormManager";
-import { SidebarData } from "../interface/sidebarInterface";
+import { SidebarData, SidebarSection } from "../interface/sidebarInterface";
 
-const dummySidebarData: SidebarData = {
+const baseSidebar: SidebarData = {
     title: "My Forms",
     footer: [
         {
@@ -14,38 +15,63 @@ const dummySidebarData: SidebarData = {
             },
         },
     ],
-    sections: [
-        {
-            "SSEH2201 - Sem 1 2024": {
-                sectionName: "SSEH2201 - Sem 1 2024",
-                sectionOnClick: () => {
-                    console.log("SSEH2201 - Sem 1 2024 clicked");
-                },
-            },
-        },
-        {
-            "SSEH3301 - Sem 2 2023": {
-                sectionName: "SSEH3301 - Sem 2 2023",
-                sectionOnClick: () => {
-                    console.log("SSEH3301 - Sem 2 2023 clicked");
-                },
-            },
-        },
-        {
-            "SSEH3301 - Sem 1 2023": {
-                sectionName: "SSEH3301 - Sem 1 2023",
-                sectionOnClick: () => {
-                    console.log("SSEH3301 - Sem 1 2023 clicked");
-                },
-            },
-        },
-    ],
+    sections: [] as SidebarSection[],
 };
 
+const dummySidebarData: SidebarSection[] = [
+    {
+        "SSEH2201 - Sem 1 2024": {
+            sectionName: "SSEH2201 - Sem 1 2024",
+            sectionOnClick: () => {
+                console.log("SSEH2201 - Sem 1 2024 clicked");
+            },
+        },
+    },
+    {
+        "SSEH3301 - Sem 2 2023": {
+            sectionName: "SSEH3301 - Sem 2 2023",
+            sectionOnClick: () => {
+                console.log("SSEH3301 - Sem 2 2023 clicked");
+            },
+        },
+    },
+    {
+        "SSEH3301 - Sem 1 2023": {
+            sectionName: "SSEH3301 - Sem 1 2023",
+            sectionOnClick: () => {
+                console.log("SSEH3301 - Sem 1 2023 clicked");
+            },
+        },
+    },
+];
+
 const StudentFormManagerPage = () => {
+    const [sidebarData, setSidebarData] = useState<SidebarData>({
+        ...baseSidebar,
+        sections: [] as SidebarSection[],
+    });
+
+    const getFormHistory = () => {
+        // TODO: Send GET request to backend to get form history (detail of forms filled out by current student)
+        // Some code here ...
+        return dummySidebarData; // TODO: Replace with actual data
+    };
+
+    const buildSidebarData = (formHistory: any) => {
+        return {
+            ...baseSidebar,
+            sections: formHistory,
+        };
+    };
+
+    useEffect(() => {
+        const formHistory = getFormHistory();
+        setSidebarData(buildSidebarData(formHistory));
+    }, []);
+
     return (
         <Layout
-            sidebarContent={dummySidebarData}
+            sidebarContent={sidebarData}
             mainContent={<StudentFormManager />}
         ></Layout>
     );
