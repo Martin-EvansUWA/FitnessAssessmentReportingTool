555eef340d226d3eb80efb1abbaccaafff418cac
Martin-EvansUWA <23621647@student.uwa.edu.au>
2024-09-27 10:29:23 +0800

    secret key comment

--- back-end/auth.py
+++ back-end/auth.py
@@ -10,7 +10,7 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-
+# fake secret key, needs to become env variable
 SECRET_KEY = "590236d89b9f9bedc2fb29ff95618ed31493080406d59ce5cded7dd210e3b88e"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 15
