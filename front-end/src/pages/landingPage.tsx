a2d274734ead296681d8d3199e2bace0c2ef5645
LloydN01 <lloydna0527@gmail.com>
2024-09-27 18:10:42 +0800

    integrate logic where if user is logged in already, they will be redirect to the correct form manager page when navigating to the landing page

--- front-end/src/pages/landingPage.tsx
+++ front-end/src/pages/landingPage.tsx
@@ -1,3 +1,6 @@
+import Cookies from "js-cookie";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import uwaSportScienceImg2 from "../assets/uwa-sport-science-2.jpg";
 import uwaSportScienceImg3 from "../assets/uwa-sport-science-3.jpg";
 import uwaSportScienceImg from "../assets/uwa-sport-science.jpg";
@@ -6,6 +9,21 @@ import NavigationBar from "../components/navigationBar";
 
 const LandingPage = () => {
     const imgCss = "object-cover w-1/3 border-8 rounded-3xl border-[#e0e0e0]";
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const access_token = Cookies.get("access_token");
+        // If the user is already logged in, redirect them to the correct page
+        if (access_token) {
+            const isAdmin = Cookies.get("isAdmin");
+            if (isAdmin === "true") {
+                navigate("/admin-form-manager");
+            } else {
+                navigate("/student-form-manager");
+            }
+        }
+    }, [navigate]);
 
     return (
         <div className="flex flex-col h-screen">
